1a57b7da1ed1b13ebf29488fba380b4323155138
Manimuthu <86956699+mani195102@users.noreply.github.com>
2025-10-25 18:29:13 +0530

    fetch netlify functions replaced

--- save-segment/src/App.jsx
+++ save-segment/src/App.jsx
@@ -63,7 +63,7 @@ function App() {
     };
 
     try {
-      const response = await fetch(netlifyFetch, {
+      const response = await fetch( /.netlify/functions/sendWebhook, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
